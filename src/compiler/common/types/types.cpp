199fe09a4285b4db3b492d61ab07ddd1c6a6c62a
Xiaoli Zhou <yihe.zxl@alibaba-inc.com>
2026-03-10 16:36:16 +0800

    support varchar(max_length) in NeuG type system
    Committed-by: Xiaoli Zhou from Dev container

--- src/compiler/common/types/types.cpp
+++ src/compiler/common/types/types.cpp
@@ -343,6 +343,19 @@ uint32_t PhysicalTypeUtils::getFixedTypeSize(PhysicalTypeID physicalType) {
   }
 }
 
+void StringTypeInfo::serializeInternal(Serializer& serializer) const {};
+
+// The method will be invoked in `operator==` of LogicalType, which will be
+// invoked after comparison of LogicalTypeId, we think the string type with
+// different max_length are equal.
+bool StringTypeInfo::operator==(const ExtraTypeInfo& other) const {
+  return true;
+}
+
+std::unique_ptr<ExtraTypeInfo> StringTypeInfo::copy() const {
+  return std::make_unique<StringTypeInfo>(max_length);
+}
+
 bool DecimalTypeInfo::operator==(const ExtraTypeInfo& other) const {
   auto otherDecimalTypeInfo = neug_dynamic_cast<const DecimalTypeInfo*>(&other);
   if (otherDecimalTypeInfo) {
@@ -744,6 +757,8 @@ static LogicalType parseUnionType(const std::string& trimmedStr,
                                   main::ClientContext* context = nullptr);
 static LogicalType parseDecimalType(const std::string& trimmedStr);
 
+static LogicalType parseStringType(const std::string& trimmedStr);
+
 bool LogicalType::isBuiltInType(const std::string& str) {
   auto trimmedStr = StringUtils::ltrim(StringUtils::rtrim(str));
   auto upperDataTypeString = StringUtils::getUpper(trimmedStr);
@@ -787,6 +802,10 @@ LogicalType LogicalType::convertFromString(const std::string& str,
   } else if (upperDataTypeString.starts_with("DECIMAL") ||
              upperDataTypeString.starts_with("NUMERIC")) {
     type = parseDecimalType(trimmedStr);
+  } else if (upperDataTypeString == "STRING") {
+    type = LogicalType::STRING();
+  } else if (upperDataTypeString.starts_with("VARCHAR")) {
+    type = parseStringType(trimmedStr);
   } else if (tryGetIDFromString(upperDataTypeString, type.typeID)) {
     type.physicalType =
         LogicalType::getPhysicalType(type.typeID, type.extraTypeInfo);
@@ -1472,6 +1491,27 @@ LogicalType parseUnionType(const std::string& trimmedStr,
   return LogicalType::UNION(parseStructTypeInfo(trimmedStr, context));
 }
 
+LogicalType parseStringType(const std::string& trimmedStr) {
+  auto leftBracketPos = trimmedStr.find('(');
+  auto rightBracketPos = trimmedStr.find_last_of(')');
+  if (leftBracketPos == std::string::npos ||
+      rightBracketPos == std::string::npos) {
+    THROW_BINDER_EXCEPTION(
+        "Invalid format for VARCHAR type, should be VARCHAR(max_length). "
+        "Given: " +
+        trimmedStr);
+  }
+  auto maxLenStr = StringUtils::ltrim(StringUtils::rtrim(trimmedStr.substr(
+      leftBracketPos + 1, rightBracketPos - leftBracketPos - 1)));
+  auto maxLen = std::strtoll(maxLenStr.c_str(), nullptr, 0);
+  if (maxLen <= 0) {
+    THROW_BINDER_EXCEPTION(
+        "The max length of string must be a positive integer. Given: " +
+        maxLenStr);
+  }
+  return LogicalType::STRING(maxLen);
+}
+
 LogicalType parseDecimalType(const std::string& trimmedStr) {
   auto leftBracketPos = trimmedStr.find_last_of('(');
   auto rightBracketPos = trimmedStr.find_last_of(')');
@@ -1512,6 +1552,20 @@ LogicalType LogicalType::DECIMAL(uint32_t precision, uint32_t scale) {
 LogicalType LogicalType::STRUCT(std::vector<StructField>&& fields) {
   return LogicalType(LogicalTypeID::STRUCT,
                      std::make_unique<StructTypeInfo>(std::move(fields)));
+}
+
+LogicalType LogicalType::STRING() { return STRING(getDefaultStringMaxLen()); }
+
+LogicalType LogicalType::STRING(size_t max_length) {
+  size_t maxLimit = getMaxStringMaxLen();
+  if (max_length > maxLimit) {
+    LOG(WARNING) << "The max length of string is greater than the maximum "
+                    "limit, the maximum limit is "
+                 << maxLimit;
+    max_length = maxLimit;
+  }
+  return LogicalType(LogicalTypeID::STRING,
+                     std::make_unique<StringTypeInfo>(max_length));
 }
 
 LogicalType LogicalType::RECURSIVE_REL(
