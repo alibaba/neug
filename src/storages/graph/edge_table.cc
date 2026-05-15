9e9ab16158f79a8922cd94f1da2818ad291b9b2f
liulx20 <519459125@qq.com>
2026-05-15 11:13:52 +0800

    fix

--- src/storages/graph/edge_table.cc
+++ src/storages/graph/edge_table.cc
@@ -56,6 +56,9 @@ void filterInvalidEdges(std::vector<vid_t>& src_lid,
       valid_flags.push_back(false);
     }
   }
+  src_lid.resize(valid_count);
+  dst_lid.resize(valid_count);
+  qecvt
 }
 
 template <typename EDATA_T, typename ARROW_COL_T>
