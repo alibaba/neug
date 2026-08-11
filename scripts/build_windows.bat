d372088812586ad6f6a9eef4ca78037936bf4761
nengli.ln <nengli.ln@alibaba-inc.com>
2026-08-11 16:48:18 +0800

    fix build scripts

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -8,5 +8,5 @@ REM Use PYTHON_EXE from environment if set, otherwise default to "python"
 if "%PYTHON_EXE%"=="" set PYTHON_EXE=python
 REM Use VCPKG_ROOT from environment if set, otherwise use default path
 if "%VCPKG_ROOT%"=="" set VCPKG_ROOT=C:\vcpkg
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static-md -DOPENSSL_ROOT_DIR=%VCPKG_ROOT%\installed\x64-windows-static-md -DBUILD_PYTHON=ON -DPython_EXECUTABLE=%PYTHON_EXE% -DPYTHON_EXECUTABLE=%PYTHON_EXE% .
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static-md -DOPENSSL_ROOT_DIR="%VCPKG_ROOT%\installed\x64-windows-static-md" -DBUILD_PYTHON=ON -DPython_EXECUTABLE="%PYTHON_EXE%" -DPYTHON_EXECUTABLE="%PYTHON_EXE%" .
 cmake --build build -j 8 --target neug neug_py_bind
