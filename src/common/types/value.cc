0ab7042ee6d6636943dc3f02d675b363e06f2561
Yu Shao <yu.shao@alibaba-inc.com>
2026-08-20 17:13:22 +0800

    use explicit prefix N ULL/VALUE

--- src/common/types/value.cc
+++ src/common/types/value.cc
@@ -855,10 +855,10 @@ rapidjson::Value Value::ToJson(const Value& value,
 void encode_value(const Value& val, Encoder& encoder) {
   const auto& type = val.type();
   if (val.IsNull()) {
-    encoder.put_byte(0);
+    encoder.put_bytes("NULL", 4);
     return;
   }
-  encoder.put_byte(1);
+  encoder.put_bytes("VALUE", 5);
   if (type.id() == DataTypeId::kInt64) {
     encoder.put_long(val.GetValue<int64_t>());
   } else if (type.id() == DataTypeId::kVarchar) {
