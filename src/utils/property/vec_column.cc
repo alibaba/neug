3b50efea3a674fff984a9b3457cf9fa7ce63f7d6
yihe.zxl <yihe.zxl@alibaba-inc.com>
2026-09-02 17:55:21 +0800

    refactor(storage): rename vector buffer size accessor

--- src/utils/property/vec_column.cc
+++ src/utils/property/vec_column.cc
@@ -318,7 +318,7 @@ size_t VecColumn::size() const {
   return offset_accessor_ ? offset_accessor_->size() : 0;
 }
 
-size_t VecColumn::buffer_capacity() const { return size_; }
+size_t VecColumn::buffer_size() const { return size_; }
 
 void VecColumn::resize(size_t new_size) {
   if (new_size <= size_) {
